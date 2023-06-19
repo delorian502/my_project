7468549f86e832e83419b829042b7fba809d4f92
delorian502 <frikazoid502@gmail.com>
2023-06-19 15:42:19 +0300

    переход между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -7,6 +7,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser,link)
     page.open()
     page.go_to_login_page()
+    time.sleep(5)
+    login_page = LoginPage(browser,browser.current_url)
+    time.sleep(5)
+    login_page.should_be_login_page()
+    
 
 def test_guest_should_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
